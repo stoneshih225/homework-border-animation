a721c25e8cff227e2a2658a51d8633ce3574699e
stoneshih225 <sam23518020@gmail.com>
2024-06-19 11:44:09 +0800

    feat: 🎸 調整全局樣式引入點，移至 main.ts 檔案引入

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,10 @@
     </div>
 </template>
 
-<!-- 全局樣式，故不使用 scoped -->
-<style lang="scss">
-@import "./styles/styles";
+<style lang="scss" scoped>
+.app {
+    position: relative;
+    overflow: hidden;
+    min-height: 100vh;
+}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import './styles/styles.scss'; // 全局樣式
 
 createApp(App).mount('#app');
