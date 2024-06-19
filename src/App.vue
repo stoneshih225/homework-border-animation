d2ad1be50028ff24bed7adc3557cf8c5f8461cea
stoneshih225 <sam23518020@gmail.com>
2024-06-19 15:37:41 +0800

    feat: 🎸 更新 App 組件，引入 Box、Button 與 Radio 組件

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
-
+import Box from './components/Box/Box.vue';
+import Button from './components/Button/Button.vue';
+import Radio from './components/Radio/Radio.vue';
+import controllerConfig from './controllerConfig';
 </script>
 
 <template>
     <div class="app">
-        <!--  -->
+        <div class="content-container">
+            <Box />
+        </div>
+        <div class="controller-container">
+            <Button :buttonList="controllerConfig.buttonList" />
+            <Radio :radioList="controllerConfig.radioList" />
+        </div>
     </div>
 </template>
 
@@ -12,6 +21,29 @@
 .app {
     position: relative;
     overflow: hidden;
+    padding: 1rem;
+    min-width: 390px;
     min-height: 100vh;
+    background-color: #000;
+
+    .content-container {
+        position: relative;
+        margin-bottom: 3rem;
+        padding-bottom: calc(100% / 358 * 358);
+        height: 100%;
+
+        @media (min-width: 576px) {
+            margin: 0 auto 3rem;
+            padding-bottom: calc(576px / 358 * 358);
+            width: 576px;
+        }
+    }
+
+    .controller-container {
+        @media (min-width: 576px) {
+            margin: 0 auto;
+            width: 576px;
+        }
+    }
 }
 </style>
