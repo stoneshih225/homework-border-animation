ef9244338733483ba442a5f4f86a0fe8de3b716b
stoneshih225 <sam23518020@gmail.com>
2024-06-19 15:33:12 +0800

    feat: 🎸 新增 Box 組件，加入走線動畫

---
+++ src/components/Box/Box.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="box-wrap">
+        <div class="box-container" />
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@import "./Box";
+</style>
