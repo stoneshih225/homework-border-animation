8af6d98c2c0b20776964e995e3a5d6c98e4a1918
stoneshih225 <sam23518020@gmail.com>
2024-06-19 10:44:59 +0800

    chore: 🛠 加入 @types/node 套件，並設定 '@' 的路徑指向 src

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { fileURLToPath, URL } from 'url';
 import vue from '@vitejs/plugin-vue';
 import eslintPlugin from '@nabla/vite-plugin-eslint';
 
@@ -7,5 +8,13 @@ export default defineConfig({
     plugins: [
         vue(),
         eslintPlugin({ eslintOptions: { cache: false } })
-    ]
+    ],
+    resolve: {
+        alias: [
+            { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) }
+        ]
+    },
+    server: {
+        host: true
+    }
 });
