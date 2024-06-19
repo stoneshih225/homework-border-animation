989f0b2db65412756e5777279ee635e05f8d2b81
stoneshih225 <sam23518020@gmail.com>
2024-06-19 15:36:04 +0800

    feat: 🎸 新增 Button 組件與 Radio 組件

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { PropType } from 'vue';
+import { ButtonList } from '@/controllerConfig';
+
+const props = defineProps({
+    buttonList: {
+        type: Array as PropType<ButtonList[]>,
+        required: true
+    }
+});
+</script>
+
+<template>
+    <div class="button-container">
+        <button
+            v-for="btn in props.buttonList"
+            :key="btn.id"
+            class="button-mode"
+        >
+            {{ btn.name }}
+        </button>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.button-container {
+    margin-bottom: 1rem;
+
+    .button-mode {
+        position: relative;
+        margin-right: 0.5rem;
+    }
+}
+</style>
---
+++ src/components/Radio/Radio.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { PropType } from 'vue';
+import { RadioList } from '@/controllerConfig';
+
+const props = defineProps({
+    radioList: {
+        type: Array as PropType<RadioList[]>,
+        required: true
+    }
+});
+</script>
+
+<template>
+    <div class="radio-container">
+        <div
+            v-for="radio in props.radioList"
+            :key="radio.id"
+            class="radio"
+        >
+            <input
+                type="radio"
+                :id="radio.name"
+            />
+            <label :for="radio.name">{{ radio.name }}</label>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.radio-container {
+    position: relative;
+    display: flex;
+
+    .radio {
+        margin-right: 0.5rem;
+        color: #fff;
+    }
+}
+</style>
