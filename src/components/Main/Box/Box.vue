3b4426c474a51243acfc0d2c6a5b04f084516d8d
stoneshih225 <sam23518020@gmail.com>
2024-06-20 16:03:01 +0800

    tidy: ♻ Box 組件搬遷至 Main 資料夾內並做調整

---
+++ src/components/Main/Box/Box.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { PropType } from 'vue';
+
+const props = defineProps({
+    shapeList: {
+        type: Array as PropType<{ id: string; name: string; }[]>,
+        required: true
+    },
+    layoutStyle: {
+        type: String,
+        required: true
+    },
+    mode: {
+        type: [String, Array] as PropType<'All' | number[]>,
+        required: true
+    }
+});
+</script>
+
+<template>
+    <div
+        v-for="(box, idx) in props.shapeList"
+        :key="box.id"
+        class="box"
+        :class="[
+            props.layoutStyle,
+            { 'border-animation': props.mode === 'All' ? true : props.mode.includes(idx) }
+        ]"
+    />
+</template>
+
+<style lang="scss" scoped>
+@import "./Box";
+</style>
