46e4f6b21bb9e37642ce249a22d4b37f64035779
stoneshih225 <sam23518020@gmail.com>
2024-10-02 18:19:24 +0800

    tidy: ♻ 移除不必要的程式碼

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import Main from './components/Main/Main.vue';
-import Button from './components/Button/Button.vue';
-import Radio from './components/Radio/Radio.vue';
-import controllerConfig from './controllerConfig';
 
 const layout = ref<1 | 3 | 5 | 10>(1);
 const animationMode = ref<'All' | 'Random'>('All');
@@ -19,26 +16,15 @@ const shape = ref<'Rectangle' | 'Heart'>('Rectangle');
                 :shape="shape"
             />
         </div>
-        <div class="controller-container">
-            <Button
-                v-model:layout="layout"
-                :buttonList="controllerConfig.layouts"
-            />
-            <Radio
-                v-model:value="animationMode"
-                :radioList="controllerConfig.animationModes"
-            />
-            <Radio
-                v-model:value="shape"
-                :radioList="controllerConfig.shapes"
-            />
-        </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
 .app {
     position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
     overflow: hidden;
     padding: 1rem;
     min-width: 390px;
@@ -47,20 +33,11 @@ const shape = ref<'Rectangle' | 'Heart'>('Rectangle');
 
     .content-container {
         position: relative;
-        margin-bottom: 3rem;
         padding-bottom: calc(100% / 358 * 358);
-        height: 100%;
+        width: 100%;
 
         @media (min-width: 576px) {
-            margin: 0 auto 3rem;
             padding-bottom: calc(576px / 358 * 358);
-            width: 576px;
-        }
-    }
-
-    .controller-container {
-        @media (min-width: 576px) {
-            margin: 0 auto;
             width: 576px;
         }
     }
